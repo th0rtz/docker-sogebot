f5f10c92859431355d20e26f3e505ae63e8d7b9f
th0rtz <me@th0rtz.fr>
2018-05-02 21:02:18 +0200

    ADD: Multiple sed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,6 +24,8 @@ sed -i "s/broadcaster_username_here.*/$SOGEBOT_BOT_BROADCASTER_USERNAME\"/g" con
 sed -i "s/broadcaster_oauth_here.*/$SOGEBOT_BOT_BROADCASTER_OAUTH\"/g" config.json
 sed -i "s/1wjn1i3792t71tl90fmyvd0zl6ri2vg.*/$SOGEBOT_BOT_CLIENTID\"/g" config.json
 sed -i "s/20000.*/$SOGEBOT_PORT,/g" config.json
+sed -i "s/\"username\": \"admin\",.*/\"username\": \"$SOGEBOT_USERNAME\",/g" config.json
+sed -i "s/\"password\": \"admin\",.*/\"password\": \"$SOGEBOT_PASSWORD\",/g" config.json
 
 sleep 60000
 
