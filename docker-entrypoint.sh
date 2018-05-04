ff79a4cfad6cb5a637ce4d31138323e171e2cb58
th0rtz <me@th0rtz.fr>
2018-05-04 08:48:09 +0200

    MOD: empty var checking

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,21 +14,29 @@ envs=(
 )
 
 #Check if var is empty and set default value if not set
+haveconfig=
+for e in "${envs[@]}"; do
+	if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
+		haveConfig=1
+	fi
+done
+
 
 #Copy the template config to the good place
 cp config.example.json config.json
 
 # Sed for replacing all the VAR
-sed -i "s/bot_username_here.*/$SOGEBOT_USERNAME\",/g" config.json
-sed -i "s/bot_oauth_here.*/$SOGEBOT_BOT_OAUTH\",/g" config.json
-sed -i "s/broadcaster_username_here.*/$SOGEBOT_BOT_BROADCASTER_USERNAME\",/g" config.json
-sed -i "s/broadcaster_oauth_here.*/$SOGEBOT_BOT_BROADCASTER_OAUTH\",/g" config.json
-sed -i "s/1wjn1i3792t71tl90fmyvd0zl6ri2vg.*/$SOGEBOT_BOT_CLIENTID\"/g" config.json
-sed -i "s/20000.*/$SOGEBOT_PORT,/g" config.json
-sed -i "s/\"username\": \"admin\",.*/\"username\": \"$SOGEBOT_USERNAME\",/g" config.json
-sed -i "s/\"password\": \"admin\",.*/\"password\": \"$SOGEBOT_PASSWORD\",/g" config.json
-sed -i "s/\"domain\": \"localhost\",.*/\"domain\": \"$SOGEBOT_DOMAIN\",/g" config.json
 
-sleep 2
+if [ "$haveConfig" ]; then
+	sed -i "s/bot_username_here.*/$SOGEBOT_USERNAME\",/g" config.json
+	sed -i "s/bot_oauth_here.*/$SOGEBOT_BOT_OAUTH\",/g" config.json
+	sed -i "s/broadcaster_username_here.*/$SOGEBOT_BOT_BROADCASTER_USERNAME\",/g" config.json
+	sed -i "s/broadcaster_oauth_here.*/$SOGEBOT_BOT_BROADCASTER_OAUTH\",/g" config.json
+	sed -i "s/1wjn1i3792t71tl90fmyvd0zl6ri2vg.*/$SOGEBOT_BOT_CLIENTID\"/g" config.json
+	sed -i "s/20000.*/$SOGEBOT_PORT,/g" config.json
+	sed -i "s/\"username\": \"admin\",.*/\"username\": \"$SOGEBOT_USERNAME\",/g" config.json
+	sed -i "s/\"password\": \"admin\",.*/\"password\": \"$SOGEBOT_PASSWORD\",/g" config.json
+	sed -i "s/\"domain\": \"localhost\",.*/\"domain\": \"$SOGEBOT_DOMAIN\",/g" config.json
 
-npm start
+	npm start
+fi
