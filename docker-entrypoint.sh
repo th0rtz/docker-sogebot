f5d6f70656584414bb75bd881ad72903cde37f79
th0rtz <me@th0rtz.fr>
2018-05-04 10:20:41 +0200

    MOD: env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 envs=(
 	SOGEBOT_PORT
-	SOGEBOT_USERNAME
-	SOGEBOT_PASSWORD
+	SOGEBOT_WEB_USERNAME
+	SOGEBOT_WEB_PASSWORD
 	SOGEBOT_BOT_NAME
 	SOGEBOT_BOT_OAUTH
 	SOGEBOT_BOT_BROADCASTER_USERNAME
@@ -28,14 +28,14 @@ cp config.example.json config.json
 # Sed for replacing all the VAR
 
 if [ "$haveConfig" ]; then
-	sed -i "s/bot_username_here.*/$SOGEBOT_USERNAME\",/g" config.json
+	sed -i "s/bot_username_here.*/$SOGEBOT_BOT_NAME\",/g" config.json
 	sed -i "s/bot_oauth_here.*/$SOGEBOT_BOT_OAUTH\",/g" config.json
 	sed -i "s/broadcaster_username_here.*/$SOGEBOT_BOT_BROADCASTER_USERNAME\",/g" config.json
 	sed -i "s/broadcaster_oauth_here.*/$SOGEBOT_BOT_BROADCASTER_OAUTH\",/g" config.json
 	sed -i "s/1wjn1i3792t71tl90fmyvd0zl6ri2vg.*/$SOGEBOT_BOT_CLIENTID\"/g" config.json
 	sed -i "s/20000.*/$SOGEBOT_PORT,/g" config.json
-	sed -i "s/\"username\": \"admin\",.*/\"username\": \"$SOGEBOT_USERNAME\",/g" config.json
-	sed -i "s/\"password\": \"admin\",.*/\"password\": \"$SOGEBOT_PASSWORD\",/g" config.json
+	sed -i "s/\"username\": \"admin\",.*/\"username\": \"$SOGEBOT_WEB_USERNAME\",/g" config.json
+	sed -i "s/\"password\": \"admin\",.*/\"password\": \"$SOGEBOT_WEB_PASSWORD\",/g" config.json
 	sed -i "s/\"domain\": \"localhost\",.*/\"domain\": \"$SOGEBOT_DOMAIN\",/g" config.json
 
 	npm start
